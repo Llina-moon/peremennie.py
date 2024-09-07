c9f4e6161e39742b66502f848fad91a7c2584f14
Llina-moon <llina.moon.luna@gmail.com>
2024-09-07 19:57:32 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,9 @@
+workhome = 12
+time = 1.5
+name = ('Python')
+time_1 = (time / workhome)
+
+print(f'Курс: {name}, всего задач : { workhome}, затрачено часов: {time}, среднее время выполнения {time_1} часов')
+
+
+
